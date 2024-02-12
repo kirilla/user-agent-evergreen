6a7262ca502d806a8b2607373d9220ac0561776d
Jonas Sundström <jonas@kirilla.se>
2024-02-12 15:03:03 +0100

    See if we can get the non-headless user-agent string.

--- scripts/run_edge.py
+++ scripts/run_edge.py
@@ -4,8 +4,9 @@ from selenium.webdriver.common.keys import Keys
 # Edge
 edge_options = webdriver.EdgeOptions()
 edge_options.add_argument("--headless")
+edge_options.use_chromium = True
+edge_options.add_argument("--disable-blink-features=AutomationControlled")
 edge_driver = webdriver.Edge(options=edge_options)
 edge_driver.get("http://localhost:8080/edge")
 print("Edge Browser Title:", edge_driver.title)
 edge_driver.quit()
-
