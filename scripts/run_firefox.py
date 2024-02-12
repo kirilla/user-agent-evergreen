8895f1c17c0952b599ec5249de43186d51a981be
Jonas Sundström <jonas@kirilla.se>
2024-02-12 15:04:56 +0100

    See if we can get the non-headless user-agent string.

--- scripts/run_firefox.py
+++ scripts/run_firefox.py
@@ -4,8 +4,9 @@ from selenium.webdriver.common.keys import Keys
 # Firefox
 firefox_options = webdriver.FirefoxOptions()
 firefox_options.add_argument("--headless")
+firefox_options.add_argument("--marionette")
+firefox_options.add_argument("--disable-blink-features=AutomationControlled")
 firefox_driver = webdriver.Firefox(options=firefox_options)
 firefox_driver.get("http://localhost:8080/firefox")
 print("Firefox Browser Title:", firefox_driver.title)
 firefox_driver.quit()
-
